adb3110f0f82e5ef9f1e0a76ef18e8b4d980e308
anthonyray <dantho361@hotmail.com>
2017-08-17 12:08:44 +0200

    beginning test suite

--- tests/utils/test_custom_asr.py
+++ tests/utils/test_custom_asr.py
@@ -1,10 +1,16 @@
 from unittest import TestCase
+from snipsskills.utils.custom_asr import CustomASR
 
 
 class BaseTest(TestCase):
-
     def setUp(self):
-        pass
+        self.customASR = CustomASR(True, '/home/pi/project/asr.tar.gz')
+
+class TestUnixCommands(BaseTest):
+    def test_generates_asr_extraction_command(self):
+        generated_cmd = self.customASR.generate_asr_archive_extraction_command()
+        self.assertEqual(generated_cmd,
+                         'sudo mkdir -p /opt/snips/asr && sudo tar xf /home/pi/project/asr.tar.gz -C /opt/snips/asr --strip-components 1')
 
 
-class Test
+
